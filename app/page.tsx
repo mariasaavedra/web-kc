4227de7262fb07bfc20b9932c215c84aa4c7cdc7
Maria Saavedra <msaav3@gmail.com>
2023-08-11 18:10:52 -0500

    chore: added social link

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,21 @@ export default async function Home() {
   const data = await response.json();
 
   return (
-    <main className="grid grid-cols-1 md:grid-cols-4 gap-4 p-8 bg-[#f6c492]">
+    <main className="grid grid-cols-1 md:grid-cols-4 gap-4 p-8 bg-[#cb997e]">
       {data &&
         data.length > 0 &&
         data.map((activity: any, i: any) => {
           return (
             <div
-              className="bg-white border rounded shadow border p-4 overflow-hidden"
+              className="bg-[#f7ede2] rounded shadow  p-4 overflow-hidden"
               key={i}
             >
               {Boolean(activity.fields.Attachments) &&
                 activity.fields.Attachments.length > 0 && (
-                  <img className="object-cover w-full h-[150px] rounded" src={activity.fields.Attachments[0].url} />
+                  <img
+                    className="object-cover w-full h-[150px] rounded"
+                    src={activity.fields.Attachments[0].url}
+                  />
                 )}
               <h3 className="font-bold my-2 text-lg">{activity.fields.Name}</h3>
               <span className="my-2 uppercase text-xs text-gray-600">
@@ -36,6 +39,17 @@ export default async function Home() {
                 className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-blue-700 text-sm my-2 max-w-[500px]"
               >
                 {activity.fields.Website}
+              </a>
+              {Boolean(activity.fields.Website) && (
+                <span className="my-2 uppercase text-xs text-gray-600">
+                  Social
+                </span>
+              )}
+              <a
+                href={`${activity.fields["Social Media"]}`}
+                className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-blue-700 text-sm my-2 max-w-[500px]"
+              >
+                {activity.fields["Social Media"]}
               </a>
             </div>
           );
