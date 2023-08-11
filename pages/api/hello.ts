813b0d5379301f99957e5e9d1b8ebb38ce15629d
Maria Saavedra <msaav3@gmail.com>
2023-08-11 16:28:31 -0500

    chore: added air table API

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,13 +3,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 const Airtable = require("airtable");
 Airtable.configure({
   endpointUrl: "https://api.airtable.com",
-  apiKey: "YOUR_SECRET_API_TOKEN",
+  apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY
 });
 const base = Airtable.base("appXn8rWR51YKBwX2");
+const table = base('Activities');
 
-export default function handler(
+export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse
 ) {
-  return res.status(200).json({ message: "hello" });
+  const records = await table.select({}).all();
+  return res.status(200).json(records);
 }
