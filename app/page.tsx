9f7a3b9bfb99575df6beaa8f96fef990a89028a3
Maria Saavedra <msaav3@gmail.com>
2023-08-11 20:38:29 -0500

    chore: fix sctuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
 import Airtable from "airtable";
+import ActivityCard from "@/components/ActivityCard";
 
 Airtable.configure({
   endpointUrl: "https://api.airtable.com",
@@ -18,70 +18,9 @@ export default async function Home() {
 
   return (
     <main className="grid grid-cols-1 md:grid-cols-4 gap-4 p-8 bg-[#9ED2BE]">
-      {data &&
-        data.length > 0 &&
-        data.map((activity: any, i: any) => {
-          return (
-            <div
-              className="bg-[#f5f5f5] rounded-xl shadow-xl  p-4 overflow-hidden"
-              key={i}
-            >
-              {Boolean(activity.fields.Attachments) &&
-                activity.fields.Attachments.length > 0 && (
-                  <img
-                    className="shadow object-cover w-full h-[150px] rounded"
-                    src={activity.fields.Attachments[0].url}
-                  />
-                )}
-              <h3 className="font-bold my-2 text-lg">{activity.fields.Name}</h3>
-              <span className="my-2 uppercase text-xs text-gray-600">
-                Details
-              </span>
-              <p className="text-sm my-2">
-                {activity.fields.Details.substring(0, 150)}
-              </p>
-              {Boolean(activity.fields.Website) && (
-                <span className="my-2 uppercase text-xs text-gray-600">
-                  Website
-                </span>
-              )}
-              <a
-                target="_blank"
-                href={`${activity.fields.Website}`}
-                className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-xs my-2 max-w-[500px]"
-              >
-                {activity.fields.Website}
-              </a>
-              {Boolean(activity.fields["Social Media"]) && (
-                <>
-                  <span className="my-2 uppercase text-xs text-gray-600">
-                    Social
-                  </span>
-                  <a
-                    target="_blank"
-                    href={`${activity.fields["Social Media"]}`}
-                    className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-xs my-2 max-w-[500px]"
-                  >
-                    {activity.fields["Social Media"]}
-                  </a>
-                </>
-              )}
-
-              {activity.fields["Kind of Activity"] &&
-                activity.fields["Kind of Activity"].length > 0 &&
-                activity.fields["Kind of Activity"].map((type, i) => {
-                  return (
-                    <span
-                      key={i}
-                      className="bg-blue-100 text-blue-800 text-xs font-medium my-2 mr-2 px-2.5 py-0.5 rounded-full 0"
-                    >
-                      {type}
-                    </span>
-                  );
-                })}
-            </div>
-          );
-        })}
+      {data?.map((activity, i) => {
+        return <ActivityCard key={i} activity={activity} />;
+      })}
     </main>
   );
 }
---
+++ components/ActivityCard.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+interface Attachment {
+  url: string;
+}
+
+interface ActivityFields {
+  Attachments?: Attachment[];
+  Name: string;
+  Details: string;
+  Website?: string;
+  SocialMedia?: string;
+  ActivityTypes?: string[];
+}
+
+interface Activity {
+  fields: ActivityFields;
+}
+
+interface ActivityCardProps {
+  activity: Activity;
+}
+
+function ActivityCard({ activity }: ActivityCardProps) {
+  const { fields } = activity;
+
+  return (
+    <div className="bg-[#f5f5f5] cursor-pointer rounded-xl shadow-xl p-4 overflow-hidden">
+      {fields.Attachments && fields.Attachments.length > 0 && (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          className="shadow object-cover w-full h-[150px] rounded"
+          src={fields.Attachments[0].url}
+          alt="Activity"
+        />
+      )}
+      <h3 className="font-bold my-2 text-lg">{fields.Name}</h3>
+
+      <span className="my-2 uppercase text-xs text-gray-600">Details</span>
+      <p className="text-sm my-2">{fields.Details.substring(0, 150)}</p>
+
+      {fields.Website && (
+        <>
+          <span className="my-2 uppercase text-xs text-gray-600">Website</span>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={fields.Website}
+            className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-xs my-2 max-w-[500px]"
+          >
+            {fields.Website}
+          </a>
+        </>
+      )}
+
+      {fields.SocialMedia && (
+        <>
+          <span className="my-2 uppercase text-xs text-gray-600">Social</span>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={fields.SocialMedia}
+            className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-xs my-2 max-w-[500px]"
+          >
+            {fields.SocialMedia}
+          </a>
+        </>
+      )}
+
+      {fields.ActivityTypes &&
+        fields.ActivityTypes.length > 0 &&
+        fields.ActivityTypes.map((type, i) => (
+          <span
+            key={i}
+            className="bg-blue-100 text-blue-800 text-xs font-medium my-2 mr-2 px-2.5 py-0.5 rounded-full"
+          >
+            {type}
+          </span>
+        ))}
+    </div>
+  );
+}
+
+export default ActivityCard;
