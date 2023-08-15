cffa2dcb97c4ea38de0aa87a45f543f23e276bd4
Maria Saavedra <msaav3@gmail.com>
2023-08-15 09:49:13 -0500

    chore: add button and fix modal styles

--- components/ActivityCard.tsx
+++ components/ActivityCard.tsx
@@ -3,7 +3,6 @@ import ActivityCardDetails from "./ActivityCardDetails";
 import Modal from "./Modal";
 import { ActivityCardProps } from "@/types";
 
-
 function ActivityCard({ activity, searchParams }: ActivityCardProps) {
   const { id, fields } = activity;
   const showActivity = searchParams?.activity; // get the id from params ex: "?activity=123"
@@ -12,9 +11,11 @@ function ActivityCard({ activity, searchParams }: ActivityCardProps) {
 
   return (
     <div className="bg-[#f5f5f5] cursor-pointer rounded-xl shadow-xl p-4 overflow-hidden">
-      <ActivityCardDetails  id={id} fields={fields} />
-      <a href={`?activity=${id}`}>View</a>
-      {id === showActivity && activity !== undefined && <Modal fields={fields}/>}
+      <ActivityCardDetails id={id} fields={fields} />
+      <a className="bg-transparent text-center text-xs block mt-8 hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded" href={`?activity=${id}`}>
+        <span>VIEW DETAILS</span>
+      </a>
+      {id === showActivity && <Modal fields={fields} />}
     </div>
   );
 }
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -19,8 +19,8 @@ export default function Modal(props: ModalProps) {
       {/* The backdrop, rendered as a fixed sibling to the panel container */}
       <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
       <div className="fixed inset-0 flex items-center justify-center p-4">
-        <Dialog.Panel className="w-1/2 h-full p-8 rounded bg-white">
-          <Link href="/">
+        <Dialog.Panel className="w-full md:w-1/2 h-full rounded bg-white">
+          <Link className="outline-0 mr-0 px-2 pt-2 block text-right " href="/">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -31,88 +31,90 @@ export default function Modal(props: ModalProps) {
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
-              className="block lucide lucide-x-square"
+              className="inline lucide lucide-x-square"
             >
               <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
               <path d="m15 9-6 6" />
               <path d="m9 9 6 6" />
             </svg>
           </Link>
-          <h3 className="inline font-bold my-2 text-lg">{fields.Name}</h3>
+          <div className="p-4">
+            <div>
+              {fields.Attachments && fields.Attachments.length > 0 && (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                  className="shadow object-cover w-full  mb-2 h-[200px] rounded"
+                  src={fields.Attachments[0].url}
+                  alt="Activity"
+                />
+              )}
 
-          <div>
-            {fields.Attachments && fields.Attachments.length > 0 && (
-              // eslint-disable-next-line @next/next/no-img-element
-              <img
-                className="shadow object-cover w-full h-[300px] rounded"
-                src={fields.Attachments[0].url}
-                alt="Activity"
-              />
-            )}
+              <h3 className="block font-bold my-2 text-lg">{fields.Name}</h3>
 
-            <span className="my-2 uppercase text-xs text-gray-600">
-              Details
-            </span>
-            <p className="text-sm pr-4  py-2 my-2 ">{fields.Details}</p>
+              <span className="my-2 uppercase text-xs text-gray-600">
+                Details
+              </span>
+              <p className="text-sm pr-4  py-2 my-2 ">{fields.Details}</p>
 
-            {fields.Website && (
-              <div>
-                <span className="my-2 uppercase text-xs text-gray-600">
-                  Website
-                </span>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={fields.Website}
-                  className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-xs my-2 max-w-[500px]"
-                >
-                  {fields.Website}
-                </a>
-              </div>
-            )}
+              {fields.Website && (
+                <div>
+                  <span className="my-2 uppercase text-xs text-gray-600">
+                    Website
+                  </span>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={fields.Website}
+                    className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-xs my-2 max-w-[500px]"
+                  >
+                    {fields.Website}
+                  </a>
+                </div>
+              )}
 
-            {fields.Instagram && (
-              <div>
-                <span className="my-2 uppercase text-xs text-gray-600">
-                  Instagram
-                </span>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={fields.Instagram}
-                  className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-xs my-2 max-w-[500px]"
-                >
-                  {fields.Instagram}
-                </a>
-              </div>
-            )}
+              {fields.Instagram && (
+                <div>
+                  <span className="my-2 uppercase text-xs text-gray-600">
+                    Instagram
+                  </span>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={fields.Instagram}
+                    className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-xs my-2 max-w-[500px]"
+                  >
+                    {fields.Instagram}
+                  </a>
+                </div>
+              )}
 
-            {fields.Facebook && (
-              <div>
-                <span className="my-2 uppercase text-xs text-gray-600">
-                  Facebook
-                </span>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={fields.Facebook}
-                  className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-xs my-2 max-w-[500px]"
-                >
-                  {fields.Facebook}
-                </a>
-              </div>
-            )}
+              {fields.Facebook && (
+                <div>
+                  <span className="my-2 uppercase text-xs text-gray-600">
+                    Facebook
+                  </span>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={fields.Facebook}
+                    className="whitespace-nowrap block font-bold overflow-hidden text-ellipsis text-xs my-2 max-w-[500px]"
+                  >
+                    {fields.Facebook}
+                  </a>
+                </div>
+              )}
 
-            {fields.ActivityTypes &&
-              fields.ActivityTypes.length > 0 &&
-              fields.ActivityTypes.map((type) => (
-                <span
-                  key={type}
-                  className="bg-blue-100 text-blue-800 text-xs font-medium my-2 mr-2 px-2.5 py-0.5 rounded-full"
-                >
-                  {type}
-                </span>
-              ))}
+              {fields.ActivityTypes &&
+                fields.ActivityTypes.length > 0 &&
+                fields.ActivityTypes.map((type) => (
+                  <span
+                    key={type}
+                    className="bg-blue-100 text-blue-800 text-xs font-medium my-2 mr-2 px-2.5 py-0.5 rounded-full"
+                  >
+                    {type}
+                  </span>
+                ))}
+            </div>
           </div>
         </Dialog.Panel>
       </div>
